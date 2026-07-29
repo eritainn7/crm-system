1de2a762cd0f5ea2514488508d5612246ea30c1d
eritainn7 <sanyapridava@mail.ru>
2026-07-29 09:57:59 +0300

    feat front with nginx in docker

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = '/api';
 
 const api: AxiosInstance = axios.create({
   baseURL: API_URL,
